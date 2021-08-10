4f0218698c20e92a05036627b3d7f5e2f0d1cc81
Eric Williams <eric.williams@contino.io>
2021-08-10 12:43:27 -0600

    Updates processor to export env vars to files

--- preprocessor.sh
+++ preprocessor.sh
@@ -6,18 +6,23 @@ echo "***************************"
 printenv | sort
 echo "***************************"
 
+echo -n "Saving current directory location of $PWD"
 pushd $PWD > /dev/null
 
+echo
 echo "Setting directory to $DOCS_SRC_PATH"
 cd $DOCS_SRC_PATH
 
-echo "Creating directory $DOCS_PROCESSED_PATH"
+echo "Ensuring and cleaning directory $DOCS_PROCESSED_PATH"
 mkdir -p $DOCS_PROCESSED_PATH
+rm $DOCS_PROCESSED_PATH/*
 
-echo "Cleaning $DOCS_PROCESSED_PATH"
-# rm $DOCS_PROCESSED/*
+echo "Saving enviornment variables as files for preprocessor"
+mkdir -p ../env/
+while IFS='=' read -r -d '' n v; do echo -n "$v" > ../env/"$n"; done < <(env -0)
 
 echo "Processing documents at $PWD into $DOCS_PROCESSED_PATH"
 find *.md -maxdepth 1 -type f -exec markdown-pp {} -o $DOCS_PROCESSED_PATH/{} \;
 
-popd > /dev/null
+echo -n "Setting directory to "
+popd
