cf721287125374c65b1010e9bccfa45503310d9b
Eric Williams <eric.williams@contino.io>
2021-08-10 23:53:11 -0600

    Updating cleaning calls

--- preprocessor.sh
+++ preprocessor.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo "***************************"
-echo "* Enviornment Variables ***"
-echo "***************************"
+echo "*************************"
+echo "* Enviornment Variables *"
+echo "*************************"
 printenv | sort
-echo "***************************"
+echo "*************************"
 
 echo -n "Saving current directory location of $PWD"
 pushd $PWD > /dev/null
@@ -15,10 +15,11 @@ cd $DOCS_SRC_PATH
 
 echo "Ensuring and cleaning directory $DOCS_PROCESSED_PATH"
 mkdir -p $DOCS_PROCESSED_PATH
-rm $DOCS_PROCESSED_PATH/*
+rm -rf $DOCS_PROCESSED_PATH/*
 
 echo "Saving enviornment variables as files for preprocessor"
 mkdir -p ../env/
+rm -rf ../env/*
 while IFS='=' read -r -d '' n v; do echo -n "$v" > ../env/"$n"; done < <(env -0)
 
 echo "Processing documents at $PWD into $DOCS_PROCESSED_PATH"
